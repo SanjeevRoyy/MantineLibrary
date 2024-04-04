dfa0d85fce9c6dceadaadb4763ec6950947683fb
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-04-04 12:46:26 +0545

    mantine form created

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Button } from "@mantine/core"
+import Form from "./components/Form/Form"
+
+function App() {
+ 
+  return (
+    <>
+   <Form/>
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import '@mantine/core/styles.css'
+import { MantineProvider } from '@mantine/core'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <MantineProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+  </MantineProvider>,
+)
