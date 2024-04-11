d4708d8de9fc48e368c88f8acd01c6a8400d01c4
SanjeevRoyy <abiralmanausya9@gmail.com>
2024-04-11 17:44:33 +0545

    added timepicker

---
+++ src/components/mantinehooks/mantinedates/TimeInput.jsx
@@ -0,0 +1,11 @@
+import { TimeInput } from '@mantine/dates';
+
+export default function Demo() {
+  return (
+    <TimeInput
+      label="Input label"
+      description="Input description"
+      placeholder="Input placeholder"
+    />
+  );
+}
